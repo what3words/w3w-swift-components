455d28983175acbc53bd0a0b4c6cd9fa9c0ecb16
Dave Duprey <dave@what3words.com>
2022-02-18 16:53:56 +0000

    changed dependancy to api-master

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    //.package(url: "https://github.com/what3words/w3w-swift-wrapper.git", "3.7.2"..<"4.0.0"),
-    .package(url: "https://github.com/what3words/w3w-swift-wrapper.git", .branch("staging"))
+    .package(url: "https://github.com/what3words/w3w-swift-wrapper.git", "3.7.2"..<"4.0.0"),
+    //.package(url: "https://github.com/what3words/w3w-swift-wrapper.git", .branch("staging"))
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
